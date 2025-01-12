a9ac464399dcb2d71c2b66fdd6db87b24cf20793
epicseven-cup <jackylin616@gmail.com>
2025-01-12 01:31:49 -0500

    doing some quick fixes on the popup and the manifest

--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,8 @@ import { setActivity, setup } from "./lib/discord.js"
 
 chrome.runtime.onInstalled.addListener((details) => {
     if (details.reason !== "install" && details.reason !== "update") return;
-    chrome.contextMenus.create({
-        "id": "sampleContextMenu",
-        "title": "Sample Context Menu",
-        "contexts": ["selection"]
-    });
+    console.info("Plugin installed / updated!")
+
 });
 
 
